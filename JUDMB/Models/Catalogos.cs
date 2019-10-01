79bc96f0d33bd06c30c1aa5dec38674cb83f0dc4
Andres Mitzi <jams45072@gmail.com>
2019-10-01 12:18:03 -0500

    MODELO de CATALOGO

---
+++ JUDMB/Models/Catalogos.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace JUDMB.Models
+{
+    public class Catalogos
+    {
+        public string Nombre_Tabla { get; set; }
+        public DataTable Data { get; set; }
+    }
+}
