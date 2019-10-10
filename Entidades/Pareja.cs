f65177ed878f205ca8f81f35ad908c2a08776185
caves117 <carloscdx117@gmail.com>
2019-10-10 11:11:51 -0500

    Ciudadano y Pareja

---
+++ Entidades/Pareja.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    class Pareja
+    {
+        public string PAR_IDPareja { get; set; }
+        public string PAR_Nombre { get; set; }
+        public string PAR_ApellidoPaterno { get; set; }
+        public string PAR_ApellidoMaterno { get; set; }
+        public int PAR_IDGenero { get; set; }
+    }
+}
